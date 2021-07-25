f3c8f142811a66bfb880b88bf9504b04e1c22993
makecent <42603768+makecent@users.noreply.github.com>
2021-07-25 21:52:03 +0800

    Delete _site directory

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome To Jekyll",
-        "excerpt":"layout: single  ","categories": [],
-        "tags": [],
-        "url": "/welcome-to-jekyll/",
-        "teaser": null
-      }]
