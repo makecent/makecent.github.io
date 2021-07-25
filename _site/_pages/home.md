700080cd5752dff02c6e923334d31605fe117881
louis-4 <luchongkai96@gmail.com>
2021-07-25 21:12:37 +0800

    local works

---
+++ _site/_pages/home.md
@@ -0,0 +1,2 @@
+## Welcome to my site
+Just for fun
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome To Jekyll",
+        "excerpt":"layout: single  ","categories": [],
+        "tags": [],
+        "url": "/welcome-to-jekyll/",
+        "teaser": null
+      }]
